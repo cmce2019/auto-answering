8395193c5cd625e3ec48f40cc188e77b159d2e90
cmce2019 <carlosm.cordoba1@correo.usa.edu.co>
2020-10-24 22:28:44 -0500

    test user_id

--- mc_questions.php
+++ mc_questions.php
@@ -91,7 +91,7 @@ class Mc_questions{
 
         $this->buyer_id=$info['body']->buyer->id;
         $this->order_id=$info['body']->id;
-
+        var_dump($info);
 /*         $ch=curl_init();
         curl_setopt($ch,CURLOPT_URL,"https://autoanswering-47a3a.firebaseio.com/auto_buymessage.json");
         curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
@@ -100,7 +100,7 @@ class Mc_questions{
         $answer_array=json_decode($response);
         curl_close($ch); */
 
-        $answer= array(
+/*         $answer= array(
             "from"=>array("user_id"=>"390630451"),
             "to"=>array("user_id"=>"658157693"),
             "text"=>"gracias"
@@ -111,7 +111,7 @@ class Mc_questions{
         $answer_data=$meli->post("/messages/packs/4093129401/sellers/390630451", $answer, $params);
 
         header("HTTP/1.1 ".$answer_data['httpCode']);
-        echo $answer_data['httpCode']==201 ?  "Se ha respondido la compra" : "No se ha respondido la compra";
+        echo $answer_data['httpCode']==201 ?  "Se ha respondido la compra" : "No se ha respondido la compra"; */
 
     }
 
