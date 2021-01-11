4331656f89f0e43072a3a2f88369231419a4d695
cmce2019 <carlosm.cordoba1@correo.usa.edu.co>
2021-01-11 15:37:38 -0500

    update httpcode refresh token

--- mc_questions.php
+++ mc_questions.php
@@ -66,10 +66,11 @@ class Mc_questions{
         $auth=$this->meli->refreshAccessToken();
 /*      $this->access_token=$auth['body']->access_token;
         $this->refresh_token=$auth['body']->refresh_token; */
+        error_log("hola");
         error_log($this->appId." ".$this->secretKey." ".$test_access_token." ".$test_refresh_token);
         error_log(json_encode($auth));
-        $this->access_token=$test_access_token." ".$test_refresh_token;
-        $this->refresh_token=2;
+        $this->access_token="APP_USR-8165220320761420-011119-a2c55a8ac59d288e01a6f76814a5ef01-390630450";
+        $this->refresh_token="TG-5ffca397be96150006913e47-390630451";
         $data='{"access_token":"'.$this->access_token.'","refresh_token":"'.$this->refresh_token.'"}';
         $ch=curl_init();
         curl_setopt($ch,CURLOPT_URL,$this->url);
